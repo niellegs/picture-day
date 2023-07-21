b03ad84cec336a5cd5be8f7f2d77919ea11d6e73
Danielle Gomes <108994881+ellegs@users.noreply.github.com>
2023-07-21 14:51:38 -0300

    home

--- static/script.js
+++ static/script.js
@@ -6,26 +6,6 @@ var date = "";
 const API_KEY = '5KdYgisReJGf8wcMqdeFMxYOZKQSyBffa3fa0XgZ';
 const base_URL = 'https://api.nasa.gov/planetary/apod?';
 
-// REQUEST APOD 
-
-// fetch(`${base_URL}?api_key=${API_KEY}`)
-// .then(res=>res.json())
-// .then(data=> {
-//     document.querySelector("main").style.background = `url("${data.url}") no-repeat fixed center`;
-// document.querySelector("main").style.backgroundSize = "cover";
-//     var title = data.title;
-
-//     container.innerHTML = (`
-//     <div id="description">
-//         <div id="content">
-//             <h1>${title.toUpperCase()}</h1>
-//             <p>${data.explanation}</p>
-//             <p>${data.date}</p>
-//         </div>
-//     </div>
-//     `)
-// }).catch(handleError);
-
 // FUNÇÕES
 
 function getDate() {
@@ -40,7 +20,7 @@ function getDate() {
     request.onload = function() {
         if(request.status === 200) {
 
-            let data = JSON.parse(request, this.responseText);
+            let data = JSON.parse(request.responseText);
             
             document.querySelector("main").style.background = `url("${data.url}") no-repeat fixed center`;
             document.querySelector("main").style.backgroundSize = "cover";
@@ -51,7 +31,6 @@ function getDate() {
                     <div id="content">
                         <h1>${title.toUpperCase()}</h1>
                         <p>${data.explanation}</p>
-                        <p>${data.date}</p>
                     </div>
                 </div>
                 `)
@@ -62,12 +41,25 @@ function getDate() {
 }
 
 function showDescription() {
-    document.getElementById("description").style.display = "block";
+    // document.querySelector("#home").style.display = "none";
+    document.querySelector("#description").style.display = "block";
     document.getElementById("content").style.display = "block";
+    
 }
 
 function closeDescription() {
     document.getElementById("description").style.display = "none";
     document.getElementById("content").style.display = "none";
+}
+
+function loadHome() {
+    container.innerHTML = "";
+    container.innerHTML += `
+    <div id="home">
+    <h1>Explore o universo.</h1>
+    <img src="./images/navezinha.png" alt="">
+    </div>
+    `;
+    document.querySelector("#home").style.backgroundColor = "rgba(227, 227, 227, .6)";
 }
 
